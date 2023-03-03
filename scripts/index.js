4357c722d4a8ad8f9213d7f3cf864cf1b5424fe1
pyresi <pyresi@yandex.ru>
2023-03-03 23:46:09 +0300

    Add active like and a new adding form

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,36 @@
-const formElement = document.querySelector('.popup__container');  
 const nameInput = document.querySelector('.popup__input_type_name'); 
 const jobInput = document.querySelector('.popup__input_type_job');
 
+const popupEdit = document.querySelector('.popup_type_edit');
+const editForm = popupEdit.querySelector('.popup__container');  
+
+const buttonEditClose = popupEdit.querySelector('.popup__button-close');
 const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonClose = document.querySelector('.popup__button-close');
-const popup = document.querySelector('.popup');
 
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
 
+const popupAdd = document.querySelector('.popup_type_add');
+const addForm = popupAdd.querySelector('.popup__container');  
+
+const buttonAdd = document.querySelector('.profile__button-add');
+const buttonAddClose = popupAdd.querySelector('.popup__button-close');
+
+
+function add() {
+    popupAdd.classList.add('popup_opened');
+}
+buttonAdd.addEventListener('click', add);
+
+
 function edit() {
     jobInput.value = profileBio.textContent;
     nameInput.value = profileName.textContent;
-    popup.classList.add('popup_opened');
+    popupEdit.classList.add('popup_opened');
 } 
 
-function close() {
-    popup.classList.remove('popup_opened');
+function close(evt) {
+    evt.target.parentNode.parentNode.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -27,8 +41,9 @@ function handleFormSubmit (evt) {
 }
 
 buttonEdit.addEventListener('click', edit);
-buttonClose.addEventListener('click', close);
-formElement.addEventListener('submit', handleFormSubmit);
+buttonEditClose.addEventListener('click', close);
+buttonAddClose.addEventListener('click', close);
+editForm.addEventListener('submit', handleFormSubmit);
 
 const elements = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#element-template').content;
@@ -40,6 +55,11 @@ function addElement(element) {
   elementTitle.innerText = element.name;
   elementPhoto.alt = element.name;
   elementPhoto.src = element.link;
+
+  currentElement.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
+
   elements.append(currentElement);
 }
 
@@ -71,4 +91,8 @@ const initialCards = [
 ];
 
 initialCards.forEach(addElement);
+
+
+
+
 
